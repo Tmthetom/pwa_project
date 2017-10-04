b48c1633292f826e67163a1b5d439b7be5f1b713
Tomáš Moravec <moravec-tomas@seznam.cz>
2017-10-04 13:06:13 +0200

    Added server project

---
+++ Server/Server/Logs/Logger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Server.Logs
+{
+    class Logger
+    {
+        public void Write(string message)
+        {
+            Console.Write("[" + DateTime.Now + "] " + message);
+        }
+
+        public void WriteLine(string message)
+        {
+            Console.Write("[" + DateTime.Now + "] " + message + Environment.NewLine);
+        }
+
+        public void WriteLine(string message, ConsoleColor textColor)
+        {
+            Console.ForegroundColor = textColor;
+            WriteLine(message);
+            Console.ResetColor();
+        }
+
+        public void IntoFile(string message)
+        {
+            ;
+        }
+    }
+}
